2ebd196896a6ebdd833114cb7dd7deb8f26d3031
Juanan Pereira <juanan.pereira@ehu.eus>
2025-02-05 12:02:31 +0100

    Initial commit

---
+++ src/main/java/eus/ehu/presentation/LoginController.java
@@ -0,0 +1,34 @@
+package eus.ehu.presentation;
+
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+
+public class LoginController {
+
+    @FXML
+    private ComboBox<String> roleCombo;
+    @FXML
+    private Label roleLabel;
+
+    private ObservableList<String> roles;
+
+    @FXML
+    void comboHandler(ActionEvent event) {
+        String role = roleCombo.getValue();
+        roleLabel.setText("You selected: " + role);
+    }
+
+    @FXML
+    void initialize() {
+        // use observable list to populate the combo box
+        roles = FXCollections.observableArrayList("Admin", "User", "Guest");
+        roleCombo.setItems(roles);
+    }
+
+
+}
